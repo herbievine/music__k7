9aae434c6299240de7bed9a1e5341b38926cff17
Herbie Vine <me@herbievine.com>
2024-11-06 16:52:13 +0100

    feat: download logic

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import { Hono } from "hono";
+import * as archiver from "archiver";
+import { Readable } from "stream";
 
 const app = new Hono();
+
+export async function getArrayBuffer(link: string, init?: RequestInit) {
+  const response = await fetch(link, init);
+
+  return response.arrayBuffer();
+}
 
 app.get("/download/:id", async (c) => {
   const id = c.req.param("id");
@@ -10,7 +18,27 @@ app.get("/download/:id", async (c) => {
     return c.json({ message: await res.text(), status: res.status }, 500);
   }
 
-  return c.json(await res.json());
+  const { artist, songs } = await res.json();
+
+  const archive = archiver("zip", { zlib: { level: 9 } });
+
+  for (const song of songs) {
+    const arrayBuffer = await getArrayBuffer(`https://audio.herbievine.com/${song.bucketId}`, {
+      headers: {
+        "content-type": "audio/mpeg",
+      },
+    });
+
+    archive.append(Buffer.from(arrayBuffer), {
+      name: `${artist.name} - ${song.name}.mp3`,
+    });
+
+    continue;
+  }
+
+  archive.finalize();
+
+  return c.body(Readable.toWeb(archive) as unknown as ReadableStream);
 });
 
 export default app;
